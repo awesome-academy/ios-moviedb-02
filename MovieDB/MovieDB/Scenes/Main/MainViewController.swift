197da048c284b11cafaa173ce3f7188f40c21262
cuonghx <cuonghx2709@gmail.com>
2019-06-17 14:23:29 +0700

    Init Project

---
+++ MovieDB/MovieDB/Scenes/Main/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  MovieDB
+//
+//  Created by cuonghx on 6/1/19.
+//  Copyright © 2019 Sun*. All rights reserved.
+//
+
+import UIKit
+
+final class MainViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    deinit {
+        logDeinit()
+    }
+}
+
+extension MainViewController: StoryboardSceneBased {
+    static var sceneStoryboard = Storyboards.main
+}
